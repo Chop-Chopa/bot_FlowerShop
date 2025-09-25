b5ab64b85cfa3b4faf525959f8f90a2e6c6ba642
Evgeny <evgenygorokhovg@yahoo.com>
2025-09-25 19:34:16 +0300

    Filled in the database.

--- admin_flowershop/admin.py
+++ admin_flowershop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ColorTheme, Category, Product, Order
 
-# Register your models here.
+
+@admin.register(ColorTheme)
+class ColorThemeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
